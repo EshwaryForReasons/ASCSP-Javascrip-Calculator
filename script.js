665231f5b7be0f663d9998a04a3a77785168b30c
EshwaryForReasons <73675827+EshwaryForReasons@users.noreply.github.com>
2021-11-19 09:07:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let firstValue = 0;
+let secondValue = 0;
+
+let bFirstValue = false;
+
+$(document).on("click", "#1", function() {
+    console.log("1")
+})
+
+$(document).on("click", "#2", function() {
+    console.log("1")
+})
+
+$(document).on("click", "#3", function() {
+    console.log("1")
+})
+
+$(document).on("click", "#4", function() {
+    console.log("1")
+})
+
+$(document).on("click", "#5", function() {
+    console.log("1")
+})
+
+$(document).on("click", "#6", function() {
+    console.log("1")
+})
+
+$(document).on("click", "#7", function() {
+    console.log("1")
+})
+
+$(document).on("click", "#8", function() {
+    console.log("1")
+})
+
+$(document).on("click", "#9", function() {
+    console.log("1")
+})
+
+$(document).on("click", "#0", function() {
+    console.log("1")
+})
